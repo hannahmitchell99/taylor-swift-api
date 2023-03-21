a4df100875e26b53a3c258153e4c76a766da28f3
Hannah <hannahamitchell99@gmail.com>
2023-03-21 17:26:36 +0200

    worked on mockdata file

---
+++ t-swift-frontend/src/data/mockData.js
@@ -0,0 +1,70 @@
+export const mockData = {
+  debut: {
+    eraID: 1,
+    albumOfEra: {
+      name: "Taylor Swift",
+      genre: "Country",
+      rating: 6.7,
+      cover: "",
+      peakChartPosition: 5,
+      numOfSongs: 11,
+      dateReleased: "24/10/2006"
+    },
+    eraDescription: "Taylor Swift's self-titled debut album was released in 2006. The album features country pop songs that were mainly written by Swift. The themes of the album focus on teenage love, heartbreak, and growing up. Swift was only 16 years old when she released this album, and her young age is reflected in the lyrics of the songs. The album was inspired by Swift's experiences in high school and her first few years in Nashville. The album's producer, Nathan Chapman, helped Swift craft a sound that combined country music with pop. The album received generally positive reviews and peaked at number five on the Billboard 200 chart.",
+    eraColorScheme: ["turquoise", "light green", "brown"],
+    eraStyle: {
+        image: "",
+        description: ["Casual", "youthful", "country", "rustic", "laidback", "twee", "prom-like"],
+        keyPieces: ["Knee-length sundresses", "cowboy boots", "sparkly minis", "tight ringlets", "chokers"]
+    },
+    eraIconicQuote: "I don't know what I want, so don't ask me.",
+    ownsMastersOfEra: false,
+    eraInspiration: ["teenage relationships", "exploring identity through the lens of relationships"]
+  },
+  fearless: {
+    eraID: 2,
+    albumOfEra: {
+      name: "Fearless",
+      genre: "Country",
+      rating: 8.1,
+      cover: "",
+      peakChartPosition: 1,
+      numOfSongs: 13,
+      dateReleased: "11/11/2008"
+    },
+    eraDescription: "",
+    eraColorScheme: ["gold", "warm yellow", "brown"],
+    eraStyle: {
+        image: "",
+        description: [ "Romance", "whimsy", "sparkles", "princess-like", "schoolgirl"],
+        keyPieces: [ "Sparkly dresses", "tulle", "softer bigger curls", "tons of bracelets", "knee-high socks", "black knee-high boots", "mini dresses"]
+    },
+    eraIconicQuote: "Back then I swore I was gonna marry him someday, but I realized some bigger dreams of mine.",
+    ownsMastersOfEra: true,
+    eraInspiration: ["growing up", "heartbreak", "teenage angst"]
+  },
+  speakNow: {
+    eraID: 3,
+    albumOfEra: {
+      name: "Speak Now",
+      genre: "Country",
+      rating: 8.2,
+      cover: "",
+      peakChartPosition: 1,
+      numOfSongs: 14,
+      dateReleased: "25/10/2010"
+    },
+    eraDescription: "",
+    eraColorScheme: ["deep purple", "mauve", "red"],
+    eraStyle: {
+        image: "",
+        description: [ "feminine", "casual", "laid-back"],
+        keyPieces: [ "graphic sweaters", "headbands", "high heels", "brown leather saddle bags"]
+    },
+    eraIconicQuote: "I am not the kind of girl who should be rudely barging in on a white veil occasion, but you are not the kind of boy who should be marrying the wrong girl.",
+    ownsMastersOfEra: false,
+    eraInspiration: ["coming of age", "heartbreak", "transition into womanhood"]
+  },
+};
+
+export default mockData;
