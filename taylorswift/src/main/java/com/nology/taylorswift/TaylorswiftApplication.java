6666fbf69ee21dadf5d9ababc11f664357ffe94a
Hannah <hannahamitchell99@gmail.com>
2023-03-22 17:31:28 +0200

    added backend files

---
+++ taylorswift/src/main/java/com/nology/taylorswift/TaylorswiftApplication.java
@@ -0,0 +1,13 @@
+package com.nology.taylorswift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaylorswiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaylorswiftApplication.class, args);
+	}
+
+}
---
+++ taylorswift/src/test/java/com/nology/taylorswift/TaylorswiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nology.taylorswift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaylorswiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
