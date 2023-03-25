137c4b825f116381f1e517e730578513785821dc
Hannah <hannahamitchell99@gmail.com>
2023-03-25 16:05:21 +0200

    added image to card

--- t-swift-frontend/src/components/EraCard/EraCard.jsx
+++ t-swift-frontend/src/components/EraCard/EraCard.jsx
@@ -1,11 +1,12 @@
 import "../EraCard/EraCard.scss"
 
-const EraCard = ({name, description, eraIconicQuote}) => {
+const EraCard = ({name, description, albumCover, eraIconicQuote}) => {
 
     return (
         <div className="era-card">
         <div className="era-card__info">
           <h2 className="era-card__info--heading">{name}</h2>
+          <img className="era-card__image" src={albumCover} alt={name} />
           <div className="era-card__info--description">
           {description} {eraIconicQuote}
           </div>
--- t-swift-frontend/src/containers/EraCardContainer/EraCardContainer.jsx
+++ t-swift-frontend/src/containers/EraCardContainer/EraCardContainer.jsx
@@ -4,7 +4,7 @@ import EraCard from "../../components/EraCard/EraCard"
 const EraCardContainer = ({eras}) => {
 
     const erasJSX = eras.map((era) => {
-        return <EraCard description = {era.eraDescription} name = {era.albumOfEra.name}  eraIconicQuote = {era.eraIconicQuote}key = {era.eraId}/>
+        return <EraCard description = {era.eraDescription} name = {era.albumOfEra.name}  eraIconicQuote = {era.eraIconicQuote} albumCover = {era.albumOfEra.cover}key = {era.eraId}/>
     })
 
   return (
