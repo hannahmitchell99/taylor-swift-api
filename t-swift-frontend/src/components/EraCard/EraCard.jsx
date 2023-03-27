8d9a61c0bf2fc910268d9586d91b7d58ee662b44
Hannah <hannahamitchell99@gmail.com>
2023-03-27 17:57:00 +0200

    partial card styling

--- t-swift-frontend/src/components/EraCard/EraCard.jsx
+++ t-swift-frontend/src/components/EraCard/EraCard.jsx
@@ -8,16 +8,16 @@ const EraCard = ({name, albumCover, genre, eraIconicQuote, inspiration}) => {
         <div className="era-card__info">
           <img className="era-card__info--image" src={albumCover} alt={name} />
           <div className="era-card__info--genre">
-            <p>Genre: {genre}</p>
+            <p>{genre}</p>
           </div>
           <div className="era-card__info--inspo">
-            <p>Inspired by {inspiration.join(", ")}</p>
+            <p className="inspiration">Inspired by {inspiration.join(", ")}</p>
           </div>
           <div className="era-card__info--quote">"{eraIconicQuote}"</div>
-          </div>
-          <div className="era-card__buttons">
+          <div className="era-card__info--buttons">
             <button className="era-card__button">quick summary</button>
             <button className="era-card__button">details, please!</button>
+          </div>
           </div>
         </div>
 
