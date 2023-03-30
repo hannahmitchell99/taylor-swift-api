06b24f315e09ffbbee67188359b00e6f2481d225
Hannah <hannahamitchell99@gmail.com>
2023-03-30 16:22:06 +0200

    added colours

--- t-swift-frontend/src/components/EraCard/EraCard.jsx
+++ t-swift-frontend/src/components/EraCard/EraCard.jsx
@@ -8,14 +8,14 @@ const EraCard = ({name, albumCover, genre, eraIconicQuote, inspiration}) => {
       <h2 className="era-card__heading">{name}</h2>
       <div className="era-card-middle">
         <div className="era-card-middle__left">
-          <img className="era-card__image" src = {albumCover}/>
+          <img className="era-card__image" src = {albumCover} alt = {name}/>
         </div>
         <div className="era-card-middle__right">
           <div className="era-card__genre">
             <p>{genre}</p>
           </div>
           <div className="era-card__inspo">
-            <p className="inspiration">Inspired by {inspiration}</p>
+            <p className="inspiration">Inspired by {inspiration.join(', ')}</p>
           </div>
         </div>
       </div>
