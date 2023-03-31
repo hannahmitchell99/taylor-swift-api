41f5f5159317b7913f40d171ea78be4f7471650b
Hannah <hannahamitchell99@gmail.com>
2023-03-31 13:27:14 +0200

    removed link styling

--- t-swift-frontend/src/components/EraCard/EraCard.jsx
+++ t-swift-frontend/src/components/EraCard/EraCard.jsx
@@ -1,36 +1,41 @@
 import { Link } from "react-router-dom";
-import "../EraCard/EraCard.scss"
+import "../EraCard/EraCard.scss";
 
-
-const EraCard = ({name, albumCover, genre, eraIconicQuote, inspiration, eraID}) => {
-
-    return (
-      <div className="era-card">
+const EraCard = ({
+  name,
+  albumCover,
+  genre,
+  eraIconicQuote,
+  inspiration,
+  eraID,
+}) => {
+  return (
+    <div className="era-card">
       <h2 className="era-card__heading">{name}</h2>
       <div className="era-card-middle">
         <div className="era-card-middle__left">
-          <img className="era-card__image" src = {albumCover} alt = {name}/>
+          <img className="era-card__image" src={albumCover} alt={name} />
         </div>
         <div className="era-card-middle__right">
           <div className="era-card__genre">
             <p>{genre}</p>
           </div>
           <div className="era-card__inspo">
-            <p className="inspiration">Inspired by {inspiration.join(', ')}</p>
+            <p className="inspiration">Inspired by {inspiration.join(", ")}</p>
           </div>
         </div>
       </div>
       <div className="era-card__quote">"{eraIconicQuote}"</div>
       <div className="era-card__buttons">
-      <Link to = {`style/${eraID}`}>
-        <button className="era-card__button">{name} era style</button>
+        <Link to={`style/${eraID}`} style={{ textDecoration: "none" }}>
+          <button className="era-card__button">{name} era style</button>
         </Link>
-        <Link to = {`/album/${eraID}`}>
-        <button className="era-card__button">{name} album details</button>
+        <Link to={`/album/${eraID}`} style={{ textDecoration: "none" }}>
+          <button className="era-card__button">{name} album details</button>
         </Link>
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default EraCard;
--- t-swift-frontend/src/pages/Album/Album.jsx
+++ t-swift-frontend/src/pages/Album/Album.jsx
@@ -1,41 +1,48 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import "../Album/Album.scss"
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+import "../Album/Album.scss";
+import { Link } from "react-router-dom";
 
-const Album = ({eras}) => {
-    const {eraID} = useParams();
-    const eraArr = eras.filter((era) => {
-        if (era.eraID == eraID) {
-          return era;
-        }
-      });
-      const targetObj = eraArr[0];
+const Album = ({ eras }) => {
+  const { eraID } = useParams();
+  const eraArr = eras.filter((era) => {
+    if (era.eraID == eraID) {
+      return era;
+    }
+  });
+  const targetObj = eraArr[0];
   return (
-        <div className="album-card">
-          <h2 className="album-card__name">{targetObj.albumOfEra.name}</h2>
-          <Link to = {`/`}>
+    <div className="album-card">
+      <h2 className="album-card__name">{targetObj.albumOfEra.name}</h2>
+      <Link to={`/`}>
         <button className="album-card__button">Home</button>
-        </Link>
-          <div className="album-card__info">
-            <div className="album-card__info--rating">
-              Pitchfork rating: {targetObj.albumOfEra.rating}
-            </div>
-            <div className="album-card__info--date">Date Released: {targetObj.albumOfEra.dateReleased}</div>
-            <div className="album-card__info--chart">
-              Peak Billboard Position: {targetObj.albumOfEra.peakChartPosition}
-            </div>
-            <div className="album-card__info--songs">
-              Number of Songs: {targetObj.albumOfEra.numOfSongs}
-            </div>
-            <div className="album-card__info--genre">Genre: {targetObj.albumOfEra.genre}</div>
-          </div>
-          <div className="album-card__cover">
-            <img className="album-card__cover--image" src={targetObj.albumOfEra.cover} alt={targetObj.albumOfEra.name}/>
-          </div>
+      </Link>
+      <div className="album-card__info">
+        <div className="album-card__info--rating">
+          Pitchfork rating: {targetObj.albumOfEra.rating}
         </div>
-      );
-    };
+        <div className="album-card__info--date">
+          Date Released: {targetObj.albumOfEra.dateReleased}
+        </div>
+        <div className="album-card__info--chart">
+          Peak Billboard Position: {targetObj.albumOfEra.peakChartPosition}
+        </div>
+        <div className="album-card__info--songs">
+          Number of Songs: {targetObj.albumOfEra.numOfSongs}
+        </div>
+        <div className="album-card__info--genre">
+          Genre: {targetObj.albumOfEra.genre}
+        </div>
+      </div>
+      <div className="album-card__cover">
+        <img
+          className="album-card__cover--image"
+          src={targetObj.albumOfEra.cover}
+          alt={targetObj.albumOfEra.name}
+        />
+      </div>
+    </div>
+  );
+};
 
-
-export default Album
+export default Album;
